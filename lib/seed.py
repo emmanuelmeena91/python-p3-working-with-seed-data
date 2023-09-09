179d97f3cb550be86edaf1b72543b7141cb071be
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-09-09 16:03:54 +0300

    complete

--- lib/seed.py
+++ lib/seed.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 from faker import Faker
-import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from models import Game
 
 fake = Faker()
 
 if __name__ == '__main__':
-    
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    session.query(Game).delete()
+
+    for _ in range(10):
+        game = Game(
+            title=fake.word(),
+            genre=fake.word(),
+            platform=fake.word(),
+            price=random.randint(10, 100),
+        )
+        session.add(game)
+
+    session.commit()
